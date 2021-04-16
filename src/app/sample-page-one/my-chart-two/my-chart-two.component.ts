e988612828586f1c432e20d0e85d264870b4d88c
orysiayashchun <54995877+orysiayashchun@users.noreply.github.com>
2021-04-16 17:26:09 +0300

    Created my-chart-two

--- src/app/sample-page-one/my-chart-two/my-chart-two.component.ts
+++ src/app/sample-page-one/my-chart-two/my-chart-two.component.ts
@@ -1,53 +1,102 @@
-import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
+
 @Component({
   selector: 'app-my-chart-two',
   templateUrl: './my-chart-two.component.html',
   styleUrls: ['./my-chart-two.component.less']
 })
-export class MyChartTwoComponent implements OnInit {
+export class MyChartTwoComponent implements AfterViewInit {
+  @ViewChild('lineCanvas') private lineCanvas?: ElementRef;
+  lineChart: any;
   public canvas:any;
   public ctx:any;
   public labels:any=['0', '27', '60', '75','90', '110', '123', '140','155','170','185','225','250'];
   public dataCases:number[]= [0, 1, 7, 3, 7, 5, 14,2,10,4,6,1,0];
+  public dataCesesNext:number[]= [0, 2, 18, 8, 13, 11, 56,15,26,11,13,3,0];
   constructor() { }
-
-  ngOnInit(): void {
-    console.log("dfg");
-    this.createChartTwo(this.labels,this.dataCases,'myChart_2');
-    console.log("dfhj");
+  ngAfterViewInit(): void {
+    if(this.lineCanvas) {
+      this.lineChartMethod(this.lineCanvas);
+    }
   }
-  private createChartTwo(labels,dataCases,chartId){
-    this.canvas=document.getElementById(chartId);
-    this.ctx=this.canvas.getContext('2d');
-
-    let chart =new Chart(this.ctx,{
-      type:'line',
-      data:{
-        labels:this.labels,
-        datasets:[{
-          data:this.dataCases,
-          backgroundColor:'#3993bb',
-          borderColor:'',
-          fill:false,
-          borderWidth:2
-        }]
-      }
-
-      // options:{
-      //   title:{
-      //     display:true,
-      //     text:"First chart"
-      //   },
-      //   tooltips:{
-      //     mode:'index',
-      //     intersect:true
-      //   },
-      //   hover:{
-      //     mode:'nearest',
-      //     intersect:true
-      //
-
+  lineChartMethod(el: ElementRef): void {
+    this.lineChart = new Chart(el.nativeElement, {
+          type:'line',
+          data:{
+            labels:this.labels,
+            datasets:[{
+              data:this.dataCases,
+              pointRadius:0,
+              tension:0.3,
+              backgroundColor:'#3993bb',
+              fill:true,
+            },
+            {
+              tension:0.3,
+              pointBorderWidth:0,
+              pointRadius:0,
+              data:[0, 2, 18, 8, 13, 11, 56,15,26,11,13,3,0],
+              backgroundColor:'#86d3ce',
+              fill:true,
+            }
+            ]
+          },
+          // options:{
+          //   responsive:false,
+          //   legend:{
+          //     display:false,
+          //   },
+          // },
     });
   }
 }
